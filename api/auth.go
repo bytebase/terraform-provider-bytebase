5c10c13b16ffdf8a02ce109cf8de1baab1412040
ecmadao <wlec@outlook.com>
2022-11-22 14:06:11 +0800

    chore: update login struct

--- api/auth.go
+++ api/auth.go
@@ -1,6 +1,12 @@
 package api
 
-// AuthResponse is the API message for user login.
+// Login is the API message for user login.
+type Login struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+// AuthResponse is the API message for user login response.
 type AuthResponse struct {
 	UserID   int    `json:"userId"`
 	Username string `json:"username"`
