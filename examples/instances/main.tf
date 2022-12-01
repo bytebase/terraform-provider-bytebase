f140ebfb99658ae90c9308003a264e558c0900dd
ecmadao <wlec@outlook.com>
2022-12-01 21:52:28 +0800

    chore: address comment

--- examples/instances/main.tf
+++ examples/instances/main.tf
@@ -2,9 +2,8 @@ terraform {
   required_providers {
     bytebase = {
       version = "0.0.3"
-      # The source is only used for local development.
-      # To use it in production, please replace it with "registry.terraform.io/bytebase/bytebase"
-      source = "terraform.local/bytebase/bytebase"
+      # For local development, please use "terraform.local/bytebase/bytebase" instead
+      source = "registry.terraform.io/bytebase/bytebase"
     }
   }
 }
