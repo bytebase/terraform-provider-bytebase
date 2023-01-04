ac7608777e917d3815d6f5b9db54265c2dde7f27
ecmadao <wlec@outlook.com>
2023-01-04 21:18:17 +0800

    chore: update tests

---
+++ provider/data_source_database_role_test.go
@@ -0,0 +1,106 @@
+package provider
+
+import (
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/bytebase/terraform-provider-bytebase/provider/internal"
+)
+
+func TestAccDatabaseRoleDataSource(t *testing.T) {
+	roleName := "test_role"
+	instanceName := "test-instance"
+	resourceName := fmt.Sprintf("data.bytebase_database_role.%s", roleName)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckRoleDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+				%s
+
+				resource "bytebase_database_role" "%s" {
+					name        = "%s"
+					instance    = bytebase_instance.%s.resource_id
+					environment = bytebase_instance.%s.environment
+
+					attribute {}
+				}
+
+				data "bytebase_database_role" "%s" {
+					name        = bytebase_database_role.%s.name
+					instance    = bytebase_instance.%s.resource_id
+					environment = bytebase_instance.%s.environment
+				}
+				`, mockInstanceResource(instanceName), roleName, roleName, instanceName, instanceName, roleName, roleName, instanceName, instanceName),
+				Check: resource.ComposeTestCheckFunc(
+					internal.TestCheckResourceExists(resourceName),
+					resource.TestCheckResourceAttr(resourceName, "name", roleName),
+					resource.TestCheckResourceAttr(resourceName, "instance", instanceName),
+					resource.TestCheckResourceAttr(resourceName, "connection_limit", "-1"),
+					resource.TestCheckResourceAttr(resourceName, "valid_until", ""),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDatabaseRoleDataSource_RoleNotFound(t *testing.T) {
+	roleName := "test_role"
+	instanceName := "test-instance"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckRoleDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+				%s
+
+				data "bytebase_database_role" "%s" {
+					name        = "%s"
+					instance    = bytebase_instance.%s.resource_id
+					environment = bytebase_instance.%s.environment
+				}
+				`, mockInstanceResource(instanceName), roleName, roleName, instanceName, instanceName),
+				ExpectError: regexp.MustCompile("Cannot found role with ID"),
+			},
+		},
+	})
+}
+
+func TestAccDatabaseRoleDataSource_InstanceNotFound(t *testing.T) {
+	roleName := "test_role"
+	instanceID := "test-instance"
+	environmentID := "test-environment"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckRoleDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+				data "bytebase_database_role" "%s" {
+					name        = "%s"
+					instance    = "%s"
+					environment = "%s"
+				}
+				`, roleName, roleName, instanceID, environmentID),
+				ExpectError: regexp.MustCompile("Cannot found role with ID"),
+			},
+		},
+	})
+}
---
+++ provider/resource_database_role_test.go
@@ -0,0 +1,137 @@
+package provider
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+
+	"github.com/bytebase/terraform-provider-bytebase/api"
+	"github.com/bytebase/terraform-provider-bytebase/provider/internal"
+)
+
+func TestAccRole(t *testing.T) {
+	roleName := "test_role"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckRoleDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(`
+				%s
+
+				resource "bytebase_database_role" "test_role_1" {
+					name        = "%s"
+					instance    = bytebase_instance.%s.resource_id
+					environment = bytebase_instance.%s.environment
+
+					attribute {}
+				}
+				`, mockInstanceResource("instance-1"), roleName, "instance-1", "instance-1"),
+				Check: resource.ComposeTestCheckFunc(
+					internal.TestCheckResourceExists("bytebase_database_role.test_role_1"),
+					resource.TestCheckResourceAttr("bytebase_database_role.test_role_1", "name", roleName),
+					resource.TestCheckResourceAttr("bytebase_database_role.test_role_1", "instance", "instance-1"),
+					resource.TestCheckResourceAttr("bytebase_database_role.test_role_1", "connection_limit", "-1"),
+					resource.TestCheckResourceAttr("bytebase_database_role.test_role_1", "valid_until", ""),
+					resource.TestCheckResourceAttr("bytebase_database_role.test_role_1", "attribute.#", "1"),
+				),
+			},
+			{
+				Config: fmt.Sprintf(`
+				%s
+
+				resource "bytebase_database_role" "test_role_2" {
+					name        = "%s"
+					instance    = bytebase_instance.%s.resource_id
+					environment = bytebase_instance.%s.environment
+
+					connection_limit = 99
+					valid_until = "2022-12-31T15:04:05+08:00"
+					attribute {}
+				}
+				`, mockInstanceResource("instance-2"), roleName, "instance-2", "instance-2"),
+				Check: resource.ComposeTestCheckFunc(
+					internal.TestCheckResourceExists("bytebase_database_role.test_role_2"),
+					resource.TestCheckResourceAttr("bytebase_database_role.test_role_2", "name", roleName),
+					resource.TestCheckResourceAttr("bytebase_database_role.test_role_2", "instance", "instance-2"),
+					resource.TestCheckResourceAttr("bytebase_database_role.test_role_2", "connection_limit", "99"),
+					resource.TestCheckResourceAttr("bytebase_database_role.test_role_2", "valid_until", "2022-12-31T15:04:05+08:00"),
+					resource.TestCheckResourceAttr("bytebase_database_role.test_role_2", "attribute.#", "1"),
+				),
+			},
+			{
+				Config: fmt.Sprintf(`
+				%s
+
+				resource "bytebase_database_role" "test_role_3" {
+					name        = "%s"
+					instance    = bytebase_instance.%s.resource_id
+					environment = bytebase_instance.%s.environment
+
+					attribute {
+						super_user  = true
+						no_inherit  = true
+						create_role = false
+					}
+				}
+				`, mockInstanceResource("instance-3"), roleName, "instance-3", "instance-3"),
+				Check: resource.ComposeTestCheckFunc(
+					internal.TestCheckResourceExists("bytebase_database_role.test_role_3"),
+					resource.TestCheckResourceAttr("bytebase_database_role.test_role_3", "name", roleName),
+					resource.TestCheckResourceAttr("bytebase_database_role.test_role_3", "instance", "instance-3"),
+					resource.TestCheckResourceAttr("bytebase_database_role.test_role_3", "attribute.#", "1"),
+					resource.TestCheckResourceAttr("bytebase_database_role.test_role_3", "attribute.0.super_user", "true"),
+					resource.TestCheckResourceAttr("bytebase_database_role.test_role_3", "attribute.0.no_inherit", "true"),
+					resource.TestCheckResourceAttr("bytebase_database_role.test_role_3", "attribute.0.create_role", "false"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckRoleDestroy(s *terraform.State) error {
+	c := testAccProvider.Meta().(api.Client)
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "bytebase_database_role" {
+			continue
+		}
+
+		environmentID, instanceID, roleName, err := internal.GetEnvironmentInstanceRoleID(rs.Primary.ID)
+		if err != nil {
+			return err
+		}
+
+		if err := c.DeleteRole(context.Background(), environmentID, instanceID, roleName); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func mockInstanceResource(resourceID string) string {
+	return fmt.Sprintf(`
+	resource "bytebase_instance" "%s" {
+		resource_id = "%s"
+		title       = "%s"
+		engine = "POSTGRES"
+		environment = "dev"
+
+		data_sources {
+			title     = "admin data source"
+			type     = "ADMIN"
+			username = "bytebase"
+			host = "127.0.0.1"
+			port = 3306
+		}
+	}
+	`, resourceID, resourceID, resourceID)
+}
