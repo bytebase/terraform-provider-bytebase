bb00eb90e16637403e7496e7f5d95358faffdb99
ecmadao <wlec@outlook.com>
2023-05-22 16:19:45 +0800

    chore: update version

--- examples/projects/main.tf
+++ examples/projects/main.tf
@@ -2,7 +2,7 @@
 terraform {
   required_providers {
     bytebase = {
-      version = "0.0.9-alpha.1"
+      version = "0.0.8-alpha.1"
       # For local development, please use "terraform.local/bytebase/bytebase" instead
       source = "registry.terraform.io/bytebase/bytebase"
     }
