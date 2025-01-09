7ff66efe75b4bc2d2080b68c503240ed8bd4d6d8
ecmadao <wlec@outlook.com>
2025-01-09 15:44:43 +0800

    chore: upgrade version

--- examples/environments/main.tf
+++ examples/environments/main.tf
@@ -2,7 +2,7 @@
 terraform {
   required_providers {
     bytebase = {
-      version = "1.0.7"
+      version = "1.0.8"
       # For local development, please use "terraform.local/bytebase/bytebase" instead
       source = "registry.terraform.io/bytebase/bytebase"
     }
