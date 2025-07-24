ff5a7bbba7aca2d47d8f479ed5255dd93e8429bf
Adela <a@bytebase.com>
2025-07-24 15:45:32 +0800

    update tf global masking (#140)

--- tutorials/8-3-global-data-masking.tf
+++ tutorials/8-3-global-data-masking.tf
@@ -15,18 +15,21 @@ resource "bytebase_policy" "global_masking_policy" {
       condition     = "column_name == \"birth_date\""
       id            = "birth-date-mask"
       semantic_type = "date-year-mask"
+      title = "Mask Birth Date Year"
     }
 
     rules {
       condition     = "column_name == \"last_name\""
       id            = "last-name-first-letter-only"
       semantic_type = "name-first-letter-only"
+      title = "Last Name Only Show First Letter"
     }
     
     rules {
       condition     = "classification_level in [\"2\"]"
       id            = "classification-level-2"
       semantic_type = "full-mask"
+      title = "Full Mask for Classification Level 2"
     }
   }
 }
