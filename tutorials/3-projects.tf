08e3d104755fbb4d1669b973c863f6ede0960070
adela <a@bytebase.com>
2025-08-18 20:55:24 +0200

    update project setting

--- tutorials/3-projects.tf
+++ tutorials/3-projects.tf
@@ -6,7 +6,22 @@ resource "bytebase_project" "project-one" {
   resource_id = "project-one"
   title       = "Project One"
 
+  allow_modify_statement = true
+  auto_resolve_issue     = false
+  auto_enable_backup     = false  
+
   databases = bytebase_instance.test.databases
+
+  webhooks {
+    title = "Sample webhook 1"
+    type  = "SLACK"
+    url   = "https://webhook.site/91fcd52a-39f1-4e7b-a43a-ddf72796d6b1"
+    notification_types = [
+      "NOTIFY_ISSUE_APPROVED",
+      "NOTIFY_PIPELINE_ROLLOUT",
+      "ISSUE_CREATE",
+    ]
+  }
 }
 
 # Project Two
