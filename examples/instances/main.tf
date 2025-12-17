30d409e6b819b7177f63f367ba43d06df3337575
ecmadao <wlec@outlook.com>
2025-12-17 22:17:23 +0800

    chore: update version

--- examples/instances/main.tf
+++ examples/instances/main.tf
@@ -2,7 +2,7 @@
 terraform {
   required_providers {
     bytebase = {
-      version = "3.11.4"
+      version = "3.13.0"
       # For local development, please use "terraform.local/bytebase/bytebase" instead
       source = "registry.terraform.io/bytebase/bytebase"
     }
